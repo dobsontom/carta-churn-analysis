15b8ae71b9df7ac8cd56789a10096dd81c848986
dobsontxx <dobs.tx@gmail.com>
2024-08-10 15:28:44 +0100

    Add customer and location CTEs, update table references

--- churn-analysis.sql
+++ churn-analysis.sql
@@ -8,23 +8,43 @@ WITH
          subscription_id,
          customer_id,
          location_id,
+         contract_value,
          subscription_start_dttm,
          subscription_end_dttm,
-         active_status,
-         DATE,
-         etc
+         CASE
+            WHEN DATE_ADD(subscription_end_dttm INTERVAL 1 MONTH) < CURRENT_DATE THEN 1
+            ELSE 0
+         END AS churn_flag,
+         DATE
+         -- etc.
       FROM
-         `data.warehouse.customer_fact`
+         `project.dataset.subscription_fact`
          JOIN start_date ON 1 = 1
       WHERE
          DATE >= dates.start_date
+   ),
+   add_customer AS (
+      SELECT
+         s.*,
+         c.customer_name,
+         c.customer_email,
+         c.customer_phone
+         -- etc.
+      FROM
+         subscription_base s
+         LEFT JOIN `project.dataset.customer_dim` c ON s.location_id = c.location_id
+   ),
+   add_location AS (
+      c.*,
+      l.country,
+      l.region,
+      l.city
+      -- etc.
+      FROM
+         add_customer c
+         LEFT JOIN `project.dataset.country_dim` ON c.location_id = l.location_id
    )
 SELECT
    *
 FROM
-   subscription_base;
-
--- customer_name,
--- customer_email,
--- customer_phone,
--- contract_value
+   add_location;
